aff711345c84b9a47b51e256c165dea681b4717b
unknown <renanmaia.rl@gmail.com>
2024-04-24 21:59:28 -0300

    identacao

--- bot.py
+++ bot.py
@@ -3,12 +3,15 @@ from bs4 import BeautifulSoup
 from telegram import Bot
 import asyncio
 
-#Buscar promoções
+siteUrl = "https://www.kabum.com.br/"
+TOKEN = "6944893922:AAF5Naf5lyINh_p33ODQee7T1Q82fLsBI2E"
+CHAT_ID = "-1002056341491"
 
+
+#Buscar promoções
 def get_promocoes(siteUrl):
     response = requests.get(siteUrl)
     soup = BeautifulSoup(response.text, 'html.parser')
-
 
     #Econtrar e extrair links
     links = soup.find_all('a', class_='sc-8932d6e-10 kjShTR productLink')
@@ -22,10 +25,6 @@ async def enviar_links_telegram(links, TOKEN, CHAT_ID):
     for link in links:
         await bot.send_message(chat_id=CHAT_ID, text=link)
 
-
-siteUrl = "https://www.kabum.com.br/"
-TOKEN = "6944893922:AAF5Naf5lyINh_p33ODQee7T1Q82fLsBI2E"
-CHAT_ID = "-1002056341491"
 
 
 async def main():
