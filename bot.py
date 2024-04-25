54554a95f6d3bf26a68093956b87073c47a68882
unknown <renanmaia.rl@gmail.com>
2024-04-24 21:53:51 -0300

    envio de links para o telegram

--- bot.py
+++ bot.py
@@ -1,37 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 from telegram import Bot
+import asyncio
 
 #Buscar promoções
 
-def buscarPromocoes(siteUrl):
+def get_promocoes(siteUrl):
     response = requests.get(siteUrl)
     soup = BeautifulSoup(response.text, 'html.parser')
 
 
     #Econtrar e extrair links
     links = soup.find_all('a', class_='sc-8932d6e-10 kjShTR productLink')
-    return print([link['href'] for link in links])
+    return [f"www.kabum.com.br{link['href']}" for link in links]
 
 
 
 #Enviar links Telegram
-async def enviarLinksTelegram(links, token, chatId):
-    bot = Bot(token)
+async def enviar_links_telegram(links, TOKEN, CHAT_ID):
+    bot = Bot(token=TOKEN) 
     for link in links:
-        await bot.send_message(chatId, link)
+        await bot.send_message(chat_id=CHAT_ID, text=link)
 
 
 siteUrl = "https://www.kabum.com.br/"
-token = "6944893922:AAF5Naf5lyINh_p330DQee7T1Q82fLsBI2E"
-chatId = "buybycodemaia"
+TOKEN = "6944893922:AAF5Naf5lyINh_p33ODQee7T1Q82fLsBI2E"
+CHAT_ID = "-1002056341491"
 
-#Buscar Promoção
-linksPromocionais = buscarPromocoes(siteUrl)
 
-    #Enviar links
 async def main():
-    await enviarLinksTelegram(linksPromocionais, token, chatId)
+    # Buscar Promoção
+    links_promocionais = get_promocoes(siteUrl)
+    await enviar_links_telegram(links_promocionais, TOKEN, CHAT_ID)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
     
 
 
