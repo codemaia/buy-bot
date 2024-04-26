da4e3cd8466ce0a9696c20d82925a3db623d2bcb
unknown <renanmaia.rl@gmail.com>
2024-04-25 23:35:41 -0300

    add status task and deployment archive

--- bot.py
+++ bot.py
@@ -1,11 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 from telegram import Bot
+from botcity.maestro import *
 import asyncio
+
 
 siteUrl = "https://www.kabum.com.br/"
 TOKEN = "6944893922:AAF5Naf5lyINh_p33ODQee7T1Q82fLsBI2E"
 CHAT_ID = "-1002056341491"
+maestro = BotMaestroSDK.from_sys_args()
+execution = maestro.get_execution()
+
+#Logion maestro
+maestro.login(
+    server="https://developers.botcity.dev",
+    login="849ab7fe-eb50-48b7-8a42-27c5679cf475",
+    key="849_DGNB6FDNJQSFXCC7SAMW"
+)
 
 
 #Buscar promoções
@@ -25,6 +36,12 @@ async def enviar_links_telegram(links, TOKEN, CHAT_ID):
     for link in links:
         await bot.send_message(chat_id=CHAT_ID, text=link)
 
+
+maestro.finish_task(
+    task_id=execution.task_id,
+    status=AutomationTaskFinishStatus.SUCCESS,
+    message="Task Finished with Success."
+)
 
 
 async def main():
